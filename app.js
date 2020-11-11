89586bbbb86b0b1514b1aab28ff5058e029e4202
unknown <mammutipoja1@gmail.com>
2020-11-11 14:53:46 +0200

    MyGallery

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const currentImage = document.querySelector('#image')
+const images = ['img/image1.jpg','img/image2.jpg','img/image3.jpg','img/image4.jpg'];
+
 function changeImage(){
   console.log("Hello from script file!");
+let rnd = Math.floor(Math.random() * images.length);
+currentImage.src = images[rnd]
+console.log(rnd);
+
 }
